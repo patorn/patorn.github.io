fa71d43826d2561ebd36bcfffd47289bc298859e
Patorn Utenpattanun <patorn.u@gmail.com>
2015-01-11 17:26:52 +0700

    Send mail with mandrill

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,40 +17,47 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
+            var m = new mandrill.Mandrill('bSzECez1dCpbl6OZWFVKHw');
+
+
+            var params = {
+              "message": {
+                "from_email": email,
+                "from_name": name,
+                "to": [{
+                  "email":"patorn.u@gmail.com",
+                  "name": "Patorn"
+                }],
+                "subject": "patorn.com Contact Form - Mandrill API: " + name,
+                "html": "You have received a new message from your website contact form.<br>Here are the details:<br>Name: " + name + "<br>Email: "+ email + "<br>Phone: " + phone + "<br>Message:<br>" + message
+              }
+            };
+
+
+
+            m.messages.send(params, function(res) {
+              // Success message
+              $('#success').html("<div class='alert alert-success'>");
+              $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                  .append("</button>");
+              $('#success > .alert-success')
+                  .append("<strong>Your message has been sent. </strong>");
+              $('#success > .alert-success')
+                  .append('</div>');
+
+              //clear all fields
+              $('#contactForm').trigger("reset");
+            }, function(err) {
+              // Fail message
+              $('#success').html("<div class='alert alert-danger'>");
+              $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                  .append("</button>");
+              $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+              $('#success > .alert-danger').append('</div>');
+              //clear all fields
+              $('#contactForm').trigger("reset");
+            });
         },
         filter: function() {
             return $(this).is(":visible");
